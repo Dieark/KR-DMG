fb217058c7f730306b85ea791695863b5ee5ed95
Dieark <57029719+Dieark@users.noreply.github.com>
2020-11-25 21:24:35 +0800

    0.0

--- js/skillTool.js
+++ js/skillTool.js
@@ -10,10 +10,10 @@ $(document).ready(function(){
 	$(document).on("keyup",".data",calc);
 	$(document).on("click",".data",calc);
 	function calc(){
-		var atk1=Number($(this).find("input:eq(0)").val());
-		var dmg1=Number($(this).find("input:eq(1)").val());
-		var atk2=Number($(this).find("input:eq(2)").val());
-		var dmg2=Number($(this).find("input:eq(3)").val());
+		var atk1=Number($(this).find(".input1").val());
+		var dmg1=Number($(this).find(".input2").val());
+		var atk2=Number($(this).find(".input3").val());
+		var dmg2=Number($(this).find(".input4").val());
 		var scale=Math.round(1000*(dmg1-dmg2)/(atk1-atk2))/1000;
 		var base=Math.ceil(dmg1-atk1*scale);
 		$(this).find(".result1").text(base);
@@ -38,5 +38,9 @@ $(document).ready(function(){
 		var edited=$(this).children().val();
 		if (edited!="")
 		$(this).html(edited+'&#x00A0<i class="fas fa-edit fa-xs"></i>');
+	});
+	
+	$(document).on("click","#toggle",function(){
+		$(".toggle").toggle();
 	});
 });
