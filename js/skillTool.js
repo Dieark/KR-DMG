c61bcd3ef3c20168afe25e2dd16cde204f1abe85
Dieark <57029719+Dieark@users.noreply.github.com>
2020-11-25 18:51:34 +0800

    0.0

--- js/skillTool.js
+++ js/skillTool.js
@@ -1,18 +1,14 @@
 $(document).ready(function(){
 	var content=$(".box").html();
 	var order=1;
-	$(document).on("click",".clean",function(){
-		$(this).parent().siblings("input").val("");
-	});
 	$(document).on("click","#plus",function(){
-		$(".box").after(content);
-		for (var i=0;i<=order;i++){
-		$("h3:eq("+i+")").text("技能"+(i+1));
-		}
+		$(content).insertBefore(".plus");
+		$("h3:eq("+order+")").text("技能"+(order+1));
 		order++;
 	});
-	$(document).on("keyup",".data",calc)
-	$(document).on("click",".data",calc)
+	
+	$(document).on("keyup",".data",calc);
+	$(document).on("click",".data",calc);
 	function calc(){
 		var atk1=Number($(this).find("input:eq(0)").val());
 		var dmg1=Number($(this).find("input:eq(1)").val());
@@ -23,4 +19,17 @@ $(document).ready(function(){
 		$(this).find(".result1").text(base);
 		$(this).find(".result2").text(scale);
 	}
+	
+	$(document).on("click",".clean",function(){
+		$(this).parent().siblings("input").val("");
+	});
+	
+	$(document).on("dblclick",".edit",function(){
+		$(this).html('<input type="text" class="form-control">');
+	});
+	$(document).on("focusout",".edit",function(){
+		var edited=$(this).children().val();
+		if (edited!="")
+		$(this).html(edited+'&#x00A0<i class="fas fa-edit fa-xs"></i>');
+	});
 });
