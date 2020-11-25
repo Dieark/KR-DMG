dd29ac88115c24c59b5ccc98c1f2b5c56a57d9f8
Dieark <57029719+Dieark@users.noreply.github.com>
2020-11-25 16:27:17 +0800

    0.0

--- js/skillTool.js
+++ js/skillTool.js
@@ -1,15 +1,26 @@
 $(document).ready(function(){
-  $("input").keyup(function(){
-	  var atk1=Number($("#input1 .form-control").val());
-	  var dmg1=Number($("#input2 .form-control").val());
-	  var atk2=Number($("#input3 .form-control").val());
-	  var dmg2=Number($("#input4 .form-control").val());
-	  var scale=Math.round(1000*(dmg1-dmg2)/(atk1-atk2))/1000;
-	  var base=Math.ceil(dmg1-atk1*scale);
-    $("#result1").text(base);
-	$("#result2").text(scale);
-  });
-  $(".clean").click(function(){
-	  $(this).parent().siblings("input").val("");
-  });
+	var content=$(".box").html();
+	var order=1;
+	$(document).on("click",".clean",function(){
+		$(this).parent().siblings("input").val("");
+	});
+	$(document).on("click","#plus",function(){
+		$(".box").after(content);
+		for (var i=0;i<=order;i++){
+		$("h3:eq("+i+")").text("技能"+(i+1));
+		}
+		order++;
+	});
+	$(document).on("keyup",".data",calc)
+	$(document).on("click",".data",calc)
+	function calc(){
+		var atk1=Number($(this).find("input:eq(0)").val());
+		var dmg1=Number($(this).find("input:eq(1)").val());
+		var atk2=Number($(this).find("input:eq(2)").val());
+		var dmg2=Number($(this).find("input:eq(3)").val());
+		var scale=Math.round(1000*(dmg1-dmg2)/(atk1-atk2))/1000;
+		var base=Math.ceil(dmg1-atk1*scale);
+		$(this).find(".result1").text(base);
+		$(this).find(".result2").text(scale);
+	}
 });
